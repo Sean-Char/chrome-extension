0aea32b1b6930caa741469e556a821d2cd09d3d2
Sean Charoutanh <scharoutanh@gmail.com>
2021-07-09 15:47:17 -0600

    add save tab button and functionality for chrome

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
+const tabBtn = document.getElementById("tab-btn")
 
 // Check if leadsFromLocalStorage is truthy
 // If so, set myLeads to its value and call renderLeads()
@@ -13,6 +12,14 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
